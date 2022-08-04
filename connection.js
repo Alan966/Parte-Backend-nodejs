5bc17f3b27c8546dce3b686bda92a3580045e28d
alanuadalupe <alanuadalupe1665@gmail.com>
2022-08-04 15:05:59 -0500

    Terminamos la aplicacion

--- connection.js
+++ connection.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 // const connection = mongoose.connect(`mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`)
-const connection = mongoose.connect(`mongodb+srv://${process.env.MONGODB_URI}@cluster0.ls7su.mongodb.net/?retryWrites=true&w=majority`)
-
+const connection = mongoose.connect(`mongodb+srv://Jairmartinez23:Jairmartinez23@cluster0.ls7su.mongodb.net/?retryWrites=true&w=majority`)
 .then(() => {
     console.log('Nos fuimos')
 }).catch( err => {
